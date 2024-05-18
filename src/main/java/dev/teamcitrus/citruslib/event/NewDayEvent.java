7ffc89aaf051a3ddd09ce6e49fd1826435704c74
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-05-18 23:11:54 +0100

    Event on New Day

---
+++ src/main/java/dev/teamcitrus/citruslib/event/NewDayEvent.java
@@ -0,0 +1,16 @@
+package dev.teamcitrus.citruslib.event;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.LevelAccessor;
+import net.neoforged.neoforge.event.level.LevelEvent;
+
+public class NewDayEvent extends LevelEvent {
+    public NewDayEvent(LevelAccessor level) {
+        super(level);
+    }
+
+    @Override
+    public ServerLevel getLevel() {
+        return (ServerLevel)super.getLevel();
+    }
+}
