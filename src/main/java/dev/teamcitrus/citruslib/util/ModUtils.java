2812b99bfba10ca44498c4c301d1928a2fae9ba8
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-05-21 14:54:36 +0100

    New util method

--- src/main/java/dev/teamcitrus/citruslib/util/ModUtils.java
+++ src/main/java/dev/teamcitrus/citruslib/util/ModUtils.java
@@ -1,9 +1,14 @@
 package dev.teamcitrus.citruslib.util;
 
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.loading.FMLLoader;
 
 public class ModUtils {
     public static boolean isDevelopmentEnvironment() {
         return !FMLLoader.isProduction();
+    }
+
+    public static boolean isModInstalled(String modID) {
+        return ModList.get().isLoaded(modID);
     }
 }
