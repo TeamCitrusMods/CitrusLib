8b7b5f23e5b7014acfaff2e52cd3188ae8331c38
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-05-26 02:04:07 +0100

    New Utility

---
+++ src/main/java/dev/teamcitrus/citruslib/util/ScreenUtil.java
@@ -0,0 +1,11 @@
+package dev.teamcitrus.citruslib.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.world.item.ItemStack;
+
+public class ScreenUtil {
+    public static boolean isItemInHand(ItemStack stack) {
+        return Minecraft.getInstance().screen instanceof AbstractContainerScreen<?> containerScreen && containerScreen.getMenu().getCarried() == stack;
+    }
+}
