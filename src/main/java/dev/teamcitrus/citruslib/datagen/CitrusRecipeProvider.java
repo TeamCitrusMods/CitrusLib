063f1bb0b61c2ba96d27d8529f508c59d7b66ed6
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-05-26 14:42:43 +0100

    Datagen recipes!

---
+++ src/main/java/dev/teamcitrus/citruslib/datagen/CitrusRecipeProvider.java
@@ -0,0 +1,77 @@
+package dev.teamcitrus.citruslib.datagen;
+
+import dev.teamcitrus.citruslib.block.WoodSet;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.Items;
+
+public abstract class CitrusRecipeProvider extends RecipeProvider {
+    public CitrusRecipeProvider(PackOutput pOutput) {
+        super(pOutput);
+    }
+
+    public void generateWoodSetRecipes(RecipeOutput recipeOutput, WoodSet woodSet) {
+        ShapedRecipeBuilder.shaped(
+                RecipeCategory.BUILDING_BLOCKS, woodSet.getStairs(), 4
+        ).define('#', woodSet.getPlanks())
+                .pattern("#  ")
+                .pattern("## ")
+                .pattern("###")
+                .group("wooden_stairs")
+                .unlockedBy("has_item", has(woodSet.getPlanks()))
+                .save(recipeOutput);
+        ShapedRecipeBuilder.shaped(
+                RecipeCategory.BUILDING_BLOCKS, woodSet.getSlab(), 6
+        ).define('#', woodSet.getPlanks())
+                .pattern("###")
+                .group("wooden_slab")
+                .unlockedBy("has_item", has(woodSet.getPlanks()))
+                .save(recipeOutput);
+        ShapedRecipeBuilder.shaped(
+                RecipeCategory.MISC, woodSet.getFence(), 4
+        ).define('#', Items.STICK).define('W', woodSet.getPlanks())
+                .pattern("W#W")
+                .pattern("W#W")
+                .group("wooden_fence")
+                .unlockedBy("has_item", has(woodSet.getPlanks()))
+                .save(recipeOutput);
+        ShapedRecipeBuilder.shaped(
+                        RecipeCategory.REDSTONE, woodSet.getFenceGate()
+                ).define('#', Items.STICK).define('W', woodSet.getPlanks())
+                .pattern("#W#")
+                .pattern("#W#")
+                .group("wooden_fence_gate")
+                .unlockedBy("has_item", has(woodSet.getPlanks()))
+                .save(recipeOutput);
+        ShapedRecipeBuilder.shaped(
+                RecipeCategory.REDSTONE, woodSet.getDoor(), 3
+        ).define('#', woodSet.getPlanks())
+                .pattern("##")
+                .pattern("##")
+                .pattern("##")
+                .group("wooden_door")
+                .unlockedBy("has_item", has(woodSet.getPlanks()))
+                .save(recipeOutput);
+        ShapedRecipeBuilder.shaped(
+                RecipeCategory.REDSTONE, woodSet.getTrapDoor(), 2
+        ).define('#', woodSet.getPlanks())
+                .pattern("###")
+                .pattern("###")
+                .group("wooden_trapdoor")
+                .unlockedBy("has_item", has(woodSet.getPlanks()))
+                .save(recipeOutput);
+        ShapedRecipeBuilder.shaped(
+                RecipeCategory.REDSTONE, woodSet.getPressurePlate()
+        ).define('#', woodSet.getPlanks())
+                .pattern("##")
+                .group("wooden_pressure_plate")
+                .unlockedBy("has_item", has(woodSet.getPlanks()))
+                .save(recipeOutput);
+        ShapelessRecipeBuilder.shapeless(
+                RecipeCategory.REDSTONE, woodSet.getButton()
+        ).requires(woodSet.getPlanks())
+                .group("wooden_button")
+                .unlockedBy("has_item", has(woodSet.getPlanks()))
+                .save(recipeOutput);
+    }
+}
