4d2880ca4661aedf502fbd8fcd1604f3cab4530c
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-07-25 20:33:08 +0100

    Add documentation

--- src/main/java/dev/teamcitrus/citruslib/event/NewDayEvent.java
+++ src/main/java/dev/teamcitrus/citruslib/event/NewDayEvent.java
@@ -4,6 +4,9 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.LevelAccessor;
 import net.neoforged.neoforge.event.level.LevelEvent;
 
+/**
+ * An event that is fired at the start of every new day (Aka at the vanilla wakeup time after sleeping)
+ */
 public class NewDayEvent extends LevelEvent {
     public NewDayEvent(LevelAccessor level) {
         super(level);
