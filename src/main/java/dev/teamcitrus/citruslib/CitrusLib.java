89646a406b76cdf8af0773ffba2d3e091e9438ea
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-10-08 23:54:55 +0100

    A fake chat component we can use

--- src/main/java/dev/teamcitrus/citruslib/CitrusLib.java
+++ src/main/java/dev/teamcitrus/citruslib/CitrusLib.java
@@ -4,6 +4,8 @@ import dev.teamcitrus.citruslib.internal.registry.BlockEntityRegistry;
 import dev.teamcitrus.citruslib.network.PayloadHelper;
 import dev.teamcitrus.citruslib.reload.ReloadListenerPayloads;
 import dev.teamcitrus.citruslib.tab.TabFillingRegistry;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -11,11 +13,14 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.ApiStatus;
 
 @Mod(CitrusLib.MODID)
 public class CitrusLib {
     public static final String MODID = "citruslib";
     public static final Logger LOGGER = LogManager.getLogger();
+
+    public static final MutableComponent FAKE_CHAT_MESSAGE = Component.translatable("message.citruslib.fake_chat");
 
     public CitrusLib(IEventBus bus) {
         bus.register(this);
@@ -31,6 +36,7 @@ public class CitrusLib {
         PayloadHelper.registerPayload(new ReloadListenerPayloads.End.Provider());
     }
 
+    @ApiStatus.Internal
     public static ResourceLocation modLoc(String id) {
         return ResourceLocation.fromNamespaceAndPath(MODID, id);
     }
