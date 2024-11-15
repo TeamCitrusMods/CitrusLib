f8c5b70985554dd8c8347df5282c33c1ae6ccc2e
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-11-15 01:34:58 +0000

    aaa

--- src/main/java/dev/teamcitrus/citruslib/internal/events/world/LevelEvents.java
+++ src/main/java/dev/teamcitrus/citruslib/internal/events/world/LevelEvents.java
@@ -7,7 +7,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.tick.LevelTickEvent;
-    
+
 @EventBusSubscriber(modid = CitrusLib.MODID)
 public class LevelEvents {
     @SubscribeEvent
