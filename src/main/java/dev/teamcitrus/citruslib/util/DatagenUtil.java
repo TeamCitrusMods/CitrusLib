195f3ea900f31a8f503ae2af4f770de007c807c9
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2025-05-20 22:46:16 +0100

    Full 21.5 update

--- src/main/java/dev/teamcitrus/citruslib/util/DatagenUtil.java
+++ src/main/java/dev/teamcitrus/citruslib/util/DatagenUtil.java
@@ -4,26 +4,8 @@ import com.google.gson.JsonElement;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.JsonOps;
 import dev.teamcitrus.citruslib.codec.CodecProvider;
-import net.minecraft.DetectedVersion;
-import net.minecraft.data.PackOutput;
-import net.minecraft.data.metadata.PackMetadataGenerator;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
-import net.minecraft.util.InclusiveRange;
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.Optional;
 
 public class DatagenUtil {
-    public static PackMetadataGenerator makeMetadataFile(PackOutput packOutput, String modID) {
-        return new PackMetadataGenerator(packOutput).add(PackMetadataSection.TYPE, new PackMetadataSection(
-                Component.literal("Resources for " + StringUtils.capitalize(modID)),
-                DetectedVersion.BUILT_IN.getPackVersion(PackType.SERVER_DATA),
-                Optional.of(new InclusiveRange<>(0, Integer.MAX_VALUE))
-        ));
-    }
-
     /**
      * Converts an object to json via codec.
      *
